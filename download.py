c37b55d496f847c1ae8c187572a6b17a270a058b
Matthew Lindner <mattlindn@gmail.com>
2015-08-08 01:37:44 -0400

    Add text output and skipping of already downloaded files

--- download.py
+++ download.py
@@ -2,14 +2,20 @@ import sys
 import argparse
 import requests
 import urllib
+import os.path
 
 
 def parse_args():
     p = argparse.ArgumentParser()
 
     p.add_argument(
-        'imgur_url', action='store',
-        help='URL to the album section to download')
+        'imgur_url', help='URL to the album section to download')
+    p.add_argument(
+        '-o', '--output', help='Destination directory to store the files')
+    p.add_argument(
+        '-c', '--continue', action="store_true",
+        help='Destination directory to store the files',
+        dest='_continue')
 
     args = p.parse_args()
 
@@ -20,19 +26,43 @@ def main(argv):
 
     args = parse_args()
 
+    output = ""
+    if args.output:
+        output = args.output
+
     url = args.imgur_url.replace('http://', 'https://')
     if url[:5] != 'https':
         url = 'https://' + url
+    print "Downloading from {}".format(url)
     url += '/page/{}.json'
 
-    for i in [1]:
-        res = requests.get(url.format(i))
+    page = 0
+    while True:
+        res = requests.get(url.format(page))
         if not res.ok:
-            raise ValueError('Bad URL: ' + url.format(i))
+            raise ValueError('Bad URL: ' + url.format(page))
         res_json = res.json()
-        for meta in res_json['data']:
+
+        data = res_json['data']
+        if not data:
+            break
+        print "Downloading page {}".format(page)
+
+        for meta in data:
             filename = meta['hash'] + meta['ext']
-            urllib.urlretrieve('https://i.imgur.com/' + filename, filename)
+            out_file = os.path.join(output, filename)
+
+            if args._continue and os.path.exists(out_file):
+                print "Found {}, skipping".format(out_file)
+                continue
+            fetch_url = 'https://i.imgur.com/' + filename
+
+            print "Writing {} to {}".format(fetch_url, out_file)
+            urllib.urlretrieve(fetch_url, out_file)
+
+        page += 1
+
+    print "Completed downloading"
 
 
 
